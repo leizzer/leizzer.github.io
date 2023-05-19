d70caa67c50ec284425c48d91124c45f9ea84950
leizzer <lizzydw@gmail.com>
2023-05-19 17:08:23 -0300

    WIP: trying out drag&drop library

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import { useDroppable } from '@dnd-kit/core'
 
-const Canvas: React.FC = () => <canvas></canvas>
+const Canvas: React.FC<React.PropsWithChildren> = (props) => {
+  const { isOver, setNodeRef } = useDroppable({
+    id: 'canvas',
+  })
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={{
+        height: 500,
+        width: 500,
+        display: 'block',
+        backgroundColor: 'lightcyan',
+        color: isOver ? 'green' : 'black',
+      }}
+    >
+      AAAAAH
+      {props.children}
+    </div>
+  )
+}
 
 export default Canvas
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Canvas from 'components/Canvas'
+import Item from 'components/Item'
 import ToolBar from 'components/ToolBar'
+import { DndContext, type DragEndEvent } from '@dnd-kit/core'
 
 const Board: React.FC = () => {
+  const [isDropped, setIsDropped] = useState(false)
+  const item = <Item>Drag me</Item>
+
+  const handleDragEnd = (event: DragEndEvent): void => {
+    if (event.over != null && event.over.id === 'canvas') {
+      setIsDropped(true)
+    }
+  }
+
   return (
-    <>
+    <DndContext onDragEnd={handleDragEnd}>
       <ToolBar />
-      <Canvas />
-    </>
+      {!isDropped ? item : null}
+      <Canvas>{isDropped ? item : 'Drop here'}</Canvas>
+    </DndContext>
   )
 }
 
